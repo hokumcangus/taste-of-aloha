fc1f93a12098d5bbe3c65cbb0aae1c088aa57ebd
hoku_mcangus <mcangusohana2@gmail.com>
2025-11-18 03:39:52 -0800

    Hoist workspace deps and cleanup nested artifacts

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,33 +1,29 @@
 import js from '@eslint/js'
-import react from "eslint-plugin-react-x";
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: tseslint.parser,
       parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
       },
     },
     rules: {
-      // Put rules you want to override here
-      "react-x/no-class-component": "warn",
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
-    },
+  },
 ])
